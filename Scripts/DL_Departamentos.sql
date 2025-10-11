9b0b486fb7ec9bfea2ee2d4bc615a0c40a6929ea
molarteve <alejandraolarte093@gmail.com>
2025-10-11 14:46:09 -0500

    Creacion de la tabla DL_Departamentos.

---
+++ Scripts/DL_Departamentos.sql
@@ -0,0 +1,7 @@
+CREATE TABLE DataLake.dbo.DL_Departamentos (
+	COD_DPTO tinyint NULL,
+	NOM_DPTO varchar(100) NULL,
+	LATITUD float NULL,
+	LONGITUD float NULL,
+	GeoDepartamento varchar(100) NULL
+);
