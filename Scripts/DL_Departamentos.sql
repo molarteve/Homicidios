077b2e7d97619d40d0d395ab4ba49321fde02752
molarteve <alejandraolarte093@gmail.com>
2025-10-11 15:01:37 -0500

    adiciono

--- Scripts/DL_Departamentos.sql
+++ Scripts/DL_Departamentos.sql
@@ -1,9 +1,7 @@
-CREATE TABLE DataLake.dbo.DL_Homicidios (
-	FECHA_HECHO date NULL,
-	COD_DEPTO tinyint NULL,
-	DEPARTAMENTO varchar(100) NULL,
-	COD_MUNI int NULL,
-	MUNICIPIO varchar(100) NULL,
-	ZONA varchar(50) NULL,
-	SEXO varchar(50) NULL,
-	CANTIDAD int NULL
+CREATE TABLE DataLake.dbo.DL_Departamentos (
+	COD_DPTO tinyint NULL,
+	NOM_DPTO varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	LATITUD float NULL,
+	LONGITUD float NULL,
+	GeoDepartamento varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+);
