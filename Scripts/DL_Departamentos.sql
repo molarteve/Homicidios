efaed118a9ecb1f1565f52658ade2c4b0651eaec
molarteve <alejandraolarte093@gmail.com>
2025-10-11 14:50:36 -0500

    Adiciono DL_Municipios

--- Scripts/DL_Departamentos.sql
+++ Scripts/DL_Departamentos.sql
@@ -1,7 +1,9 @@
-CREATE TABLE DataLake.dbo.DL_Departamentos (
-	COD_DPTO tinyint NULL,
-	NOM_DPTO varchar(100) NULL,
-	LATITUD float NULL,
-	LONGITUD float NULL,
-	GeoDepartamento varchar(100) NULL
-);
+CREATE TABLE DataLake.dbo.DL_Homicidios (
+	FECHA_HECHO date NULL,
+	COD_DEPTO tinyint NULL,
+	DEPARTAMENTO varchar(100) NULL,
+	COD_MUNI int NULL,
+	MUNICIPIO varchar(100) NULL,
+	ZONA varchar(50) NULL,
+	SEXO varchar(50) NULL,
+	CANTIDAD int NULL
