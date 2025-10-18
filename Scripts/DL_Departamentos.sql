d4044f1dcc46fdb6df223f8e1db0b30e6942ae5e
Alejandra Olarte <alejandraolarte093@gmail.com>
2025-10-17 20:57:24 -0500

    Update DL_Departamentos.sql

--- Scripts/DL_Departamentos.sql
+++ Scripts/DL_Departamentos.sql
@@ -1,7 +1,7 @@
 CREATE TABLE DataLake.dbo.DL_Departamentos (
 	COD_DPTO tinyint NULL,
-	NOM_DPTO varchar(50) NULL,
+	NOM_DPTO varchar(56) NULL,
 	LATITUD float NULL,
 	LONGITUD float NULL,
-	GeoDepartamento varchar(56) NULL
+	GeoDepartamento varchar(32) NULL
 );
